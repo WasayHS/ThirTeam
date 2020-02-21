3d03272d216e87e33f9944fd6ecdfc54811b9e54
dawsonnod <60671313+dawsonnod@users.noreply.github.com>
2020-02-20 23:08:49 -0700

    Add files via upload

---
+++ Enemy.java
@@ -0,0 +1,62 @@
+
+public class Enemy {
+	private int health = 25;
+	private int str = 1;
+	private int mag = 1;
+	private Location location;
+	
+	public Enemy() {
+		this.health = 25;
+		this.str = 1;
+		this.mag = 1;
+		this.location = new location(0, 0);
+		
+	}
+	
+//Getters
+	public int getHealth() {
+		return health;
+	}
+	
+	public int getStr() {
+		return str;
+	}
+	
+	public int getMag() {
+		return mag;
+	}
+	
+	public Location getLocation() {
+		return location;
+	}
+	
+//setters
+	public void setHP(int change) {
+		health = health+change;
+	}
+	
+	public void setStr(int s) {
+		str = s;
+	}
+	
+	public void setMag(int m) {
+		mag = m;
+	}
+	
+	public void setLocation(Location l) {
+		location = l;
+	}
+	
+	public boolean isAlive() {
+		if(health > 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+
+	}
+	
+	
+	
+}
