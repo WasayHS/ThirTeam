774d534d7c4770529a02f42f4ed8f280f0b3b421
j-felicio <60671293+j-felicio@users.noreply.github.com>
2020-02-27 20:30:09 -0700

    Replaced with NewPlayer

--- Player.java
+++ Player.java
@@ -1,12 +1,29 @@
 
-public class Enemy extends UnitType {
+public class Player extends UnitType {
 	
-	public Enemy(int health, int str, int mag, int def){
-		super();
-		this.health = health;
-		this.str = str;
-		this.mag = mag;
-		this.def = def;
+	public Player(){
+	super();
+	this.location = new Location(4,2);
 	}
-
+	
+	// Jose: I put the stat raising methods here. Can't do it in another class as it will be undefined for Player class and it's the Player stats we need to increase when user chooses a collectible.  
+	
+	public int addStr() { // Method to raise strength when user picks item that increases strength. 
+		str += 3; 
+		return str;
+		
+	}
+	
+	public int addMag() { // Method to raise magic when user picks item that increases magic.
+		mag += 1;
+		return mag;
+		
+	}
+	
+	public int addDef() { // Method to raise defense when user picks item that increases defense.
+		def += 2;
+		return def;
+	}
+	
+	
 }
