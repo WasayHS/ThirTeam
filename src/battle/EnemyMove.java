5016b44cc67db2aa520aa47bdcc4bd643429a2ed
bonnie-wu <bonnie.wu@ucalgary.ca>
2020-03-28 19:48:18 -0600

    Changed from code in Master

--- src/battle/EnemyMove.java
+++ src/battle/EnemyMove.java
@@ -1,63 +1,3 @@
-<<<<<<< HEAD:src/battle/EnemyMove.java
-package battle;
-
-import java.util.Random;
-
-import javafx.scene.layout.GridPane;
-import map.MapSetup;
-import map.Position;
-import unit.Enemy;
-
-public class EnemyMove {
-	GridPane grid;
-	public EnemyMove(GridPane g){
-		this.grid = g;
-	}
-	
-	public void move(Position playerPos ){
-		boolean moveSide, upDown;
-		Random r = new Random();
-		moveSide = r.nextBoolean();
-		if (!moveSide){
-			upDown = true;
-		}else{
-			upDown = false;
-		}
-		int chY = -1, chX = -1;
-		int endY = 1, endX = 1;
-		int moveX = 0, moveY =0;
-		if(playerPos.getX() == 1){// checks boundries
-			chX = 0;
-		}else if(playerPos.getY() == 1){
-			chY = 0;
-		}else if(playerPos.getY() == 1 && playerPos.getX() == 1){
-			chX = 0; chY = 0;
-		}else if(playerPos.getX() == 7){
-			endX = 0;
-		}else if(playerPos.getY() == 7){
-			endY = 0;
-		}else if(playerPos.getY() == 7 && playerPos.getX() == 7){
-			endX = 0; endY = 0;
-		}
-		for (int xc = chX; xc<=endX; xc++){
-			for (int yc = chY; yc<=endY; yc++){
-				if (MapSetup.getEnemy(playerPos.getX()+xc,playerPos.getY()+xc) != null){
-					Enemy enemyMoves = MapSetup.getEnemy(playerPos.getX()+xc,playerPos.getY()+xc);
-					if (upDown){
-						moveX = playerPos.getX()+xc;
-						moveY = playerPos.getY();
-					}else if (moveSide){
-						moveY = playerPos.getY()+yc;
-						moveX = playerPos.getX();
-					}
-					Position newPos = new Position(moveX,moveY);
-					MapSetup.moveUnit(grid, enemyMoves, newPos);
-				}
-			}
-		}
-	}
-}
-=======
 package battle;
 
 import java.util.Random;
@@ -135,4 +75,3 @@ public class EnemyMove {
 		return;
 	}
 }
->>>>>>> master:GUI/battle/EnemyMove.java
