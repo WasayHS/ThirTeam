f9d4eb889e50ca8bbf1bd5c9111ae729fffe9438
bonnie-wu <bonnie.wu@ucalgary.ca>
2020-03-29 18:34:28 -0600

    Update from master

--- src/application/GameState.java
+++ src/application/GameState.java
@@ -3,66 +3,68 @@ package application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import map.MapSetup;
-import map.Position;
-import unit.Player;
 
 public class GameState {
-
+	public static int mapSize = 9;
 	public void victory(Stage finished) throws Exception{
 		
 		StackPane GFinishedLayout = new StackPane();
 		Scene scene = new Scene (GFinishedLayout, 800, 450);
 		finished.setTitle("GameCompleted");
-		
+		/*
 		try{
 			Image gameCompleted  = new Image("completedGame.png");
 			GFinishedLayout.getChildren().add(new ImageView(gameCompleted));
 		}catch (IllegalArgumentException i){
-			System.err.println("Error: \"gameover.png\" not found");
+			System.err.println("Error: \"completedGame.png\" not found");
 		}
-		
+		*/
 		Button end = new Button();
 		end.setText("End Game");
 		end.setOnAction(e -> System.exit(0));
 		GFinishedLayout.getChildren().add(end);
-		
 		finished.setScene(scene);
 		finished.show();
 	}
 	
-	public void gameOver(Stage endGame) throws Exception{
+	public static void gameOver(Stage endGame) throws Exception{
 		endGame.setTitle("Game over");
+<<<<<<< HEAD:src/application/GameState.java
 		StackPane GOLayout = new StackPane();
 		GOLayout.setAlignment(Pos.CENTER);
 		Scene scene = new Scene (GOLayout, 800, 450);
+=======
+		Pane GOLayout = new Pane();
+		
+		/*
+>>>>>>> master:GUI/application/GameState.java
 		try{
 			Image gameOver = new Image("gameover.png");
 			GOLayout.getChildren().add(new ImageView(gameOver));
 		}catch (IllegalArgumentException i){
 			System.err.println("Error: \"gameover.png\" not found");
 		}
-		
+		*/
 		Button end = new Button();
 		end.setText("End Game");
-		end.setOnAction(e -> System.exit(0));
+		end.setOnAction(e -> System.exit(1));
+		end.setTranslateX(165);
+		end.setTranslateY(230);
+		
 		
 		Button play = new Button();
 		play.setText("Play Again");
-		play.setOnAction(event -> System.exit(0)); // is supposed to restart game
+		play.setOnAction(e ->SceneChange.setMorality(endGame)); // is supposed to restart game
+		play.setTranslateX(265);
+		play.setTranslateY(230);
 		
-		HBox layout = new HBox (33);
-		layout.getChildren().add(play);
-		layout.getChildren().add(end);
 		
+		GOLayout.getChildren().add(play);
+		GOLayout.getChildren().add(end);
+		Scene scene = new Scene (GOLayout, 500, 500);
 		endGame.setScene(scene);
 		endGame.show();
 	}
